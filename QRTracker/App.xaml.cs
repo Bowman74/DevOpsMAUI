eb130d1e0596864d05f01ce626ff767b4079319d
Kevin E. Ford <4380198+Bowman74@users.noreply.github.com.>
2025-03-26 12:05:41 -0700

    End of 04_04

--- QRTracker/App.xaml.cs
+++ QRTracker/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using QRTracker.Constants;
+﻿using NewRelic.MAUI.Plugin;
+using QRTracker.Constants;
 using QRTracker.ViewModels;
 using System.Diagnostics.CodeAnalysis;
 
@@ -14,6 +15,7 @@ public partial class App : Application
 
         _MainApplicationViewModel = mainWindowViewModel;    
         RegisterRoutes();
+        //InitializeNewRelic();
     }
 
     public void RegisterRoutes()
@@ -25,5 +27,23 @@ public partial class App : Application
     protected override Window CreateWindow(IActivationState? activationState)
     {
         return new MainAppWindow(_MainApplicationViewModel);
+    }
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+#if ANDROID || IOS
+        CrossNewRelic.Current.HandleUncaughtException();
+        CrossNewRelic.Current.TrackShellNavigatedEvents();
+
+        if (DeviceInfo.Current.Platform == DevicePlatform.Android)
+        {
+            CrossNewRelic.Current.Start("AA52d55d7ccd625cd835f754b73458b6b724169c5d-NRMA");
+        }
+        else if (DeviceInfo.Current.Platform == DevicePlatform.iOS)
+        {
+            CrossNewRelic.Current.Start("AA016ea8f9365e5dbb11908aee885d8110e407ef86-NRMA");
+        }
+#endif
     }
 }
